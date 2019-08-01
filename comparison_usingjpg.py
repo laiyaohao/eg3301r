83e3ba8271347e49cf5374475fca48e62f87ecd6
lyhkcuf <48747323+lyhkcuf@users.noreply.github.com>
2019-08-01 17:44:58 +0800

    Update comparison_usingjpg.py

--- comparison_usingjpg.py
+++ comparison_usingjpg.py
@@ -3,11 +3,11 @@ from gdal import Open
 from comparison import comparison
 
 # Open a before image and get its only band.
-before_ndvi = Open(r'town_area_ndvi_float32.tif') #input desired image file name into x
+before_ndvi = Open(r'officeoutside_ndvi_float32.tif') #input desired image file name into x
 before_ndvi_band = before_ndvi.GetRasterBand(1)
 
 # Open a after image and get its only band.
-after_ndvi = Open(r'central_catchment_area_vs_urban_area_ndvi_float32.tif') #input desired image file name into y
+after_ndvi = Open(r'outsideoffice_v2_ndvi_float32.tif') #input desired image file name into y
 after_ndvi_band = after_ndvi.GetRasterBand(1)
 
 # Get the rows and cols from one of the images (both should always be the same)
