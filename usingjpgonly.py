75936a46dc5fb9a88ffdd48ac365a95e4258ff74
lyhkcuf <48747323+lyhkcuf@users.noreply.github.com>
2019-08-01 13:37:00 +0800

    Update usingjpgonly.py

--- usingjpgonly.py
+++ usingjpgonly.py
@@ -14,14 +14,12 @@ red_band = red_tiff.GetRasterBand(1)
 rows, cols, geotransform = nir_tiff.RasterYSize, nir_tiff.RasterXSize, nir_tiff.GetGeoTransform()
 print(geotransform)
 
-# Set an output for a 16-bit unsigned integer (0-255)
-out_tiff_int16 = r'town_area_ndvi_int16.jpg'
+
 
 # Set the output for a 32-bit floating point (-1 to 1)
 out_tiff_float32 = r'town_area_ndvi_float32.tif'
 
-# Run the function for unsigned 16-bit integer
-ndvi(nir_band, red_band, rows, cols, geotransform, out_tiff_int16, gdal.GDT_UInt16)
+
 
 # Run the function for 32-bit floating point
 ndvi(nir_band, red_band, rows, cols, geotransform, out_tiff_float32, gdal.GDT_Float32)
