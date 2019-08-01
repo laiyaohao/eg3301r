391fc928270d1eb8a9d879b4a89b32b5f2224eef
lyhkcuf <48747323+lyhkcuf@users.noreply.github.com>
2019-08-01 13:58:15 +0800

    Create comparison_usingjpg.py

---
+++ comparison_usingjpg.py
@@ -0,0 +1,23 @@
+import gdal
+from gdal import Open
+from comparison import comparison
+
+# Open a before image and get its only band.
+before_ndvi = Open(r'town_area_ndvi_float32.tif') #input desired image file name into x
+before_ndvi_band = before_ndvi.GetRasterBand(1)
+
+# Open a after image and get its only band.
+after_ndvi = Open(r'town_area_ndvi_float32_v2.tif') #input desired image file name into y
+after_ndvi_band = after_ndvi.GetRasterBand(1)
+
+# Get the rows and cols from one of the images (both should always be the same)
+rows, cols = before_ndvi.RasterYSize, before_ndvi.RasterXSize
+
+
+
+# Run the function for 32-bit floating point
+comparison(before_ndvi_band, after_ndvi_band, rows, cols)
+
+
+
+print('done')
